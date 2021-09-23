47a909ad6e545b04bfb35a26c462f7b3be2e86d4
Kiadtisak Lowongsa <ktstomember@gmail.com>
2021-09-23 15:33:31 +0700

    Created router and app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import GlobalStyle from './utils/style/GlobalStyle';
+import MainAppBar from './components/MainAppBar';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './Route/Router';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <GlobalStyle />
+        <MainAppBar />
+        <Router />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/constants/routePaths.tsx
@@ -0,0 +1,5 @@
+export const INDEX = '/';
+export const MOVIE_DETAIL = '/:movie_id';
+export const RESULT_LIST = '/search';
+export const WATCHED_LIST = '/watched';
+export const FAVORITE_LIST = '/favorite';
